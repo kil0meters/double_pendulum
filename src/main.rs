dba96ca199ee78b9ff3186a284546b76d4900f9c
kilometers <kilo@meters.sh>
2022-03-22 01:27:04 -0700

    commit message

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use rand::Rng;
 use std::f64::consts::PI;
 
 use ppm::PPM;
 
 mod ppm;
+
+// SAFETY: it's safe trust me
+static mut L1: f64 = 1.1;
+static mut L2: f64 = 1.1;
+static mut M1: f64 = 1.1;
+static mut M2: f64 = 1.1;
+static mut G: f64 = 1.1;
 
 #[derive(Debug)]
 struct Position {
@@ -15,37 +23,36 @@ impl Position {
         (self.x - other.x).abs() < threshold && (self.y - other.y).abs() < threshold
     }
 }
-
 struct DoublePendulum {
     l1: f64,  // Length of 1st pendulum
     l2: f64,  // Length of 2nd pendulum
     a1: f64,  // Angle of 1st pendulum
     a1v: f64, // Velocity of 1st pendulum
-    a1a: f64, // Acceleration of 1st pendulum
     a2: f64,  // Angle of 2nd pendulum with respect to 1st
     a2v: f64, // Velocity of 2nd pendulum
-    a2a: f64, // Acceleration of 2nd pendulum
     m1: f64,  // Mass of 1st pendulum
     m2: f64,  // Mass of 2nd pendulum
     g: f64,   // Force of gravity
 }
 
+const SPEED: f64 = 0.05;
+
 impl DoublePendulum {
     fn new() -> Self {
-        Self {
-            l1: 10.0,
-            l2: 10.0,
+        unsafe {
+            Self {
+                l1: L1,
+                l2: L2,
 
-            a1: 0.0,
-            a1v: 0.0,
-            a1a: 0.0,
-            a2: 0.0,
-            a2v: 0.0,
-            a2a: 0.0,
+                a1: 0.0,
+                a1v: 0.0,
+                a2: 0.0,
+                a2v: 0.0,
 
-            m1: 1.0,
-            m2: 2.0,
-            g: 9.8,
+                m1: M1,
+                m2: M2,
+                g: G,
+            }
         }
     }
 
@@ -64,7 +71,6 @@ impl DoublePendulum {
     }
 
     // Returns acceleration of angle 2
-    #[allow(dead_code)]
     fn dda2(&self) -> f64 {
         let (l1, l2, a1, a1v, a2, a2v, m1, m2, g) = (
             self.l1, self.l2, self.a1, self.a1v, self.a2, self.a2v, self.m1, self.m2, self.g,
@@ -78,16 +84,14 @@ impl DoublePendulum {
     }
 
     fn advance(&mut self) {
-        self.a1a = self.dda1();
-        // this is obviously wrong but the simulation errors out immediately
-        // if I use the right function
-        self.a2a = self.dda1();
+        let a1a = self.dda1();
+        let a2a = self.dda2();
 
-        self.a1v += self.a1a;
-        self.a2v += self.a2a;
+        self.a1v += a1a * SPEED;
+        self.a2v += a2a * SPEED;
 
-        self.a1 += self.a1v;
-        self.a2 += self.a2v;
+        self.a1 += self.a1v * SPEED;
+        self.a2 += self.a2v * SPEED;
     }
 
     fn get_position(&self) -> (Position, Position) {
@@ -101,8 +105,8 @@ impl DoublePendulum {
     }
 }
 
-const WIDTH: usize = 512 * 8;
-const HEIGHT: usize = 512 * 8;
+const WIDTH: usize = 512;
+const HEIGHT: usize = 512;
 
 fn interp(x: f64, y: f64, part: f64) -> f64 {
     x + (y - x) * part
@@ -115,7 +119,10 @@ fn wow(x: usize, y: usize) -> (u8, u8, u8) {
 
     let (initial_p1, initial_p2) = double_pendulum.get_position();
 
-    for i in 0..255 {
+    // let initial_a1 = double_pendulum.a1;
+    // let initial_a2 = double_pendulum.a2;
+
+    for i in 0..(25555) {
         double_pendulum.advance();
 
         let (p1, p2) = double_pendulum.get_position();
@@ -125,7 +132,7 @@ fn wow(x: usize, y: usize) -> (u8, u8, u8) {
             let g = (i * 9) & 0xff;
             let b = (0xff - (i * 4)) & 0xff;
 
-            return (r, g, b);
+            return (r as u8, g as u8, b as u8);
         }
     }
 
@@ -133,7 +140,30 @@ fn wow(x: usize, y: usize) -> (u8, u8, u8) {
 }
 
 fn main() {
-    let mut image = PPM::new(WIDTH, HEIGHT);
-    image.color_with(wow);
-    image.write("./image.ppm").unwrap();
+    let mut rng = rand::thread_rng();
+
+    for i in 0..100 {
+        println!("Generating image {}", i);
+
+        // SAFETY: This is safe because these variables are only ever modified
+        // when there are no other threads running. Threads are only creating
+        // during the execution of the synchronous `color_with` runction.
+        unsafe {
+            L1 = rng.gen_range(0.0..100.0);
+            L2 = rng.gen_range(0.0..100.0);
+            M1 = rng.gen_range(0.0..100.0);
+            M2 = rng.gen_range(0.0..100.0);
+            G = rng.gen_range(0.0..20.0);
+
+            let mut image = PPM::new(WIDTH, HEIGHT);
+
+            image.color_with(wow);
+            image
+                .write(&format!(
+                    "./out/{} L1={} L2={} M1={} M2={} G={}.ppm",
+                    i, L1, L2, M1, M2, G
+                ))
+                .unwrap();
+        }
+    }
 }
